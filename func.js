a093a77361efef4cca50f93ff0a07a26f882edc5
Gamaia <gamaiagamer@outlook.com>
2023-11-28 21:49:47 -0300

    quarta

--- func.js
+++ func.js
@@ -31,9 +31,33 @@ function curtir(){
         botaoLike.value = "10000";
     }
 }
-/*const ApagaItens = document.querySelector('#apagaPost');
+const ApagaItens = document.querySelector('#apagaPost');
 const ApagaPerfil = document.querySelector('#apagaPerfil');
 const ApagaPhoto = document.querySelector('#photo')
-ApagaItens.style.display="none";
-ApagaPerfil.style.display="none";
-ApagaPhoto.style.display="none";*/
+const botaoNuvem = document.querySelector('#nuvemBotao')
+
+const virMenu = document.querySelector('#chamarMenu')
+const DivMenu = document.querySelector('#MenuMesmo')
+const Burguer = document.querySelector('#voltaMenu')
+
+
+DivMenu.classList.add('ReturnMenu')
+Burguer.addEventListener('click' , vaiMenu)
+virMenu.addEventListener('click' , vemMenu)
+function vemMenu(){
+    DivMenu.classList.remove('positionMenu')
+    DivMenu.classList.add('ReturnMenu')
+    ApagaItens.style.display="block";
+    ApagaPerfil.style.display="block";
+    ApagaPhoto.style.display="block";
+    botaoNuvem.style.display="block";
+    
+}
+function vaiMenu(){
+    DivMenu.classList.add('positionMenu')
+    DivMenu.classList.remove('ReturnMenu')
+    ApagaItens.style.display="none";
+    ApagaPerfil.style.display="none";
+    ApagaPhoto.style.display="none";
+    botaoNuvem.style.display="none";
+}
